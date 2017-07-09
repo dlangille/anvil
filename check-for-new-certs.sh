6cbe0dc3c6b8705f9cc1ef9a59d156bc123d620e
Dan Langille <dan.langille+git@gmail.com>
2017-07-09 15:50:55 -0400

    First commit

---
+++ check-for-new-certs.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+CERT_DST="/usr/local/etc/ssl"
+
+CERT_SERVER="https://certs.example.org/certs"
+
+MYCERTS="example.com"
+SERVICES="apache24"
+DOWNLOAD_DIR="/var/db/check-for-new-certs"
+
+# be sure to specify the agument & have no spaces in between the single quotes
+USER_AGENT="--user-agent='Check-For-New-Certificate'"
+
+CONFIG="/usr/local/etc/check-for-new-certs/check-for-new-certs.conf"
+
+if [ -f ${CONFIG} ]; then
+  . ${CONFIG}
+fi
+
+BASENAME="/usr/bin/basename"
+CP="/bin/cp"
+FETCH="/usr/bin/fetch --mirror --quiet -v ${USER_AGENT}"
+FIND_NEW_FILES="/usr/bin/find ${DOWNLOAD_DIR} -mtime 1 -type f"
+LOGGER="/usr/bin/logger"
+MV="/bin/mv"
+SERVICE="/usr/sbin/service"
+SUDO="/usr/local/bin/sudo"
+TOUCH="/usr/bin/touch"
+
+# first, we fetch the certs are looking for.
+cd ${DOWNLOAD_DIR}
+
+for cert in ${MYCERTS}
+do
+  FILES_FETCHING="ca.cer ${cert}.cer ${cert}.fullchain.cer"
+  for file in ${FILES_FETCHING}
+  do
+    ${FETCH} ${CERT_SERVER}/${cert}/${file}
+  done
+done
+
+# for each new thing we find, move it over to the right place
+NEW_FILES=`${FIND_NEW_FILES}`
+for new_file in ${NEW_FILES}
+do
+  filename=`${BASENAME} ${new_file}`
+  ${SUDO} ${CP} -a ${new_file} ${CERT_DST}/${filename}.tmp
+  ${SUDO} ${MV} ${CERT_DST}/${filename}.tmp ${CERT_DST}/${filename}
+done
+
+for service in ${SERVICES}
+do
+  case ${service} in
+    "apache24")
+      ${SUDO} ${SERVICE} ${service} graceful
+      ;;
+
+    "dovecot"|"postfix")
+      ${SUDO} ${SERVICE} ${service} restart
+      ;;
+      
+    *)
+      ${LOGGER} "Unknown service requested in $0: ${service}"
+      ;;
+  esac
+done
